2d12ed8f7709410011887f68fa494348d6a609d8
Antoine PETIT <129684995+PiouPiou974@users.noreply.github.com>
2024-11-25 12:15:02 +0400

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PPMretriever import api
 
 folder_path = ''
-excel_file = rf'{folder_path}\PPM_parcelles_en_forets.xlsx'
-tab_name = 'Parcelles_en_foret'
+excel_file = rf'{folder_path}\parcelles.xlsx'
+tab_name = 'parcelles'
 column = 'idu'
 plots = api.get_plots(excel_file_path=excel_file, tab_name=tab_name, column_name=column)
 api.fetch_and_save(plots=plots, folder_path=folder_path)
